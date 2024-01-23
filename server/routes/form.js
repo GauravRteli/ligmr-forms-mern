a5e4f3ad47c01b4488b9c664ce67e06ab50d0cfa
Harshil Sarariya <harshilprajapati9192@gmail.com>
2024-01-23 16:01:24 +0530

    api done

---
+++ server/routes/form.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const client = require("../db");
+
+router.post("/applyForm", async (req, res) => {
+  try {
+    client.connect();
+    const db = client.db("ligmr-form");
+    const collection = db.collection("posts");
+    const result = await collection.insertOne(req.body);
+
+    return res.send({ success: true, result });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.close();
+  }
+});
+
+module.exports = router;
