04f8e214f2cb9675f520b040a30cc4b3ad539076
GauravRteli <gauravteli134@gmail.com>
2024-01-23 16:15:20 +0530

    done email modeul

--- server/index.js
+++ server/index.js
@@ -7,16 +7,11 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000/"],
   })
 );
 
 app.use("/api/forms", require("./routes/form"));
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  })
-);
 
 app.listen(port, () => {
   console.log("Port is listining on " + port);
