6b0e0ae9d194119ad974d8320a13b95194e30fc5
Harshil Sarariya <harshilprajapati9192@gmail.com>
2024-01-23 16:05:09 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
-
+var cors = require("cors");
 const port = 5002;
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
 
 app.use("/api/forms", require("./routes/form"));
 
